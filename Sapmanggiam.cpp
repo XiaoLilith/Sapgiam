1c1a6570fc731819120a75121b82a53844b9c75d
XiaoLilith <125671807+XiaoLilith@users.noreply.github.com>
2023-03-08 00:00:44 +0700

    Add files via upload

---
+++ Sapmanggiam.cpp
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+#include <stdlib.h>
+void   Sapmanggiam (int X[],int n){
+	for(int i=0; i<n-1;i++){
+		int solonnhat=i;
+		for(int j=i+1;j<n;j++){
+			if(x[j]>x[solonnhat])
+			solonnhat=j;
+		}
+int main() system("x"); {
+	int a[20], n;
+	do{printf("nhap vao so luong phan tu (0<n<20) :");
+	scanf("%d,&n")
+}while(n<1 !! n>20);
+for(int i=0; i<n;i++){
+	printf("a[%D]="i)
+	scanf("%d",&a[i]);
+}
+	printf("\n mang giam dan");
+	Sapxepmangiamdan(a ,n);
+}	printMang(a, n);
+}
+}
+
